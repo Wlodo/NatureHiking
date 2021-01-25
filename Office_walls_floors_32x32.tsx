9df06d28de925805255116c546ba46e9087ddd97
Wlodo <77634878+Wlodo@users.noreply.github.com>
2021-01-25 19:10:36 +0100

    new wall

---
+++ Office_walls_floors_32x32.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Office_walls_floors_32x32" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="Office_walls_floors_32x32.png" width="512" height="384"/>
+</tileset>
